667a05c8edbd9b363581247934a84b3f4711d0de
Kendrick Tan <kendricktan0814@gmail.com>
2019-06-10 16:38:03 +1000

    bug fixes

--- poc/contracts/deploy.js
+++ poc/contracts/deploy.js
@@ -1,9 +1,10 @@
 const Web3 = require('web3');
+const fs = require('fs')
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 
-const contractAbi = [{"outputs": [], "inputs": [{"type": "bytes32", "name": "_name"}], "constant": false, "payable": false, "type": "constructor"}, {"name": "verify", "outputs": [], "inputs": [{"type": "bytes32", "name": "_bytes"}], "constant": false, "payable": false, "type": "function", "gas": 35295}, {"name": "getTestValue", "outputs": [{"type": "uint256", "name": "out"}], "inputs": [], "constant": false, "payable": false, "type": "function", "gas": 513}, {"name": "name", "outputs": [{"type": "bytes32", "name": "out"}], "inputs": [], "constant": true, "payable": false, "type": "function", "gas": 543}, {"name": "verified", "outputs": [{"type": "bool", "name": "out"}], "inputs": [], "constant": true, "payable": false, "type": "function", "gas": 573}, {"name": "testValue", "outputs": [{"type": "uint256", "name": "out"}], "inputs": [], "constant": true, "payable": false, "type": "function", "gas": 603}];
-const contractBytecode = "0x740100000000000000000000000000000000000000006020526f7fffffffffffffffffffffffffffffff6040527fffffffffffffffffffffffffffffffff8000000000000000000000000000000060605274012a05f1fffffffffffffffffffffffffdabf41c006080527ffffffffffffffffffffffffed5fa0e000000000000000000000000000000000060a05260206102256101403934156100a157600080fd5b61014051600055600060015561020d56600035601c52740100000000000000000000000000000000000000006020526f7fffffffffffffffffffffffffffffff6040527fffffffffffffffffffffffffffffffff8000000000000000000000000000000060605274012a05f1fffffffffffffffffffffffffdabf41c006080527ffffffffffffffffffffffffed5fa0e000000000000000000000000000000000060a0526375e3661660005114156100bd57602060046101403734156100b457600080fd5b61014051600255005b63a420e5d260005114156100e35734156100d657600080fd5b60025460005260206000f3005b6306fdde0360005114156101095734156100fc57600080fd5b60005460005260206000f3005b63bbb82d89600051141561012f57341561012257600080fd5b60015460005260206000f3005b638af5de72600051141561015557341561014857600080fd5b60025460005260206000f3005b60006000fd5b6100b261020d036100b26000396100b261020d036000f3";
+const contractAbi = JSON.parse(fs.readFileSync("hs.abi", "utf-8"));
+const contractBytecode = fs.readFileSync("hs.bytecode", "utf-8").toString("hex").replace("\n", "").replace(" ", "");
 
 
 (async () => {
--- poc/contracts/interact.js
+++ poc/contracts/interact.js
@@ -1,19 +1,27 @@
 const Web3 = require('web3');
+const fs = require('fs');
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 
-const contractAddress = "0x60d834624a75c60453008a140142152f0d15ade7"
-const contractAbi = [{"outputs": [], "inputs": [{"type": "bytes32", "name": "_name"}], "constant": false, "payable": false, "type": "constructor"}, {"name": "verify", "outputs": [], "inputs": [{"type": "bytes32", "name": "_bytes"}], "constant": false, "payable": false, "type": "function", "gas": 35295}, {"name": "getTestValue", "outputs": [{"type": "uint256", "name": "out"}], "inputs": [], "constant": false, "payable": false, "type": "function", "gas": 513}, {"name": "name", "outputs": [{"type": "bytes32", "name": "out"}], "inputs": [], "constant": true, "payable": false, "type": "function", "gas": 543}, {"name": "verified", "outputs": [{"type": "bool", "name": "out"}], "inputs": [], "constant": true, "payable": false, "type": "function", "gas": 573}, {"name": "testValue", "outputs": [{"type": "uint256", "name": "out"}], "inputs": [], "constant": true, "payable": false, "type": "function", "gas": 603}];
+const contractAddress = "0x007D066Bd98829d74d25aF5fAf8816e938df4C8c"
+const contractAbi = JSON.parse(fs.readFileSync("hs.abi", "utf-8"));
+
 
 (async () => {
     const accounts = await web3.eth.getAccounts();
 
     const contractInstance = new web3.eth.Contract(contractAbi, contractAddress);
 
-    const sentData = await contractInstance.methods.verify("0xd29707323fca6e8ef1270b3fb3098d3e716cf60af5e4307b2ce86587b839dcb9").send({from: accounts[0]});
+    const sentData = await contractInstance.methods.verify("0x49a3b989cc30a4d6dabc7fb7758e65176a1bbe1f6d70de61cd3f73ae81690b71")
+                        .send({from: accounts[0], gas: '1500000', gasPrice: '30000000000'})
+                        .catch(err => console.log(`${err}`));
         
-    const callData = await contractInstance.methods.getTestValue().call();
+    const listData = await contractInstance.methods.getTestList().call();
+    const testData = await contractInstance.methods.getTestValue().call();
 
-    console.log(`Value: `);
-    console.log(callData)
+    console.log(`TestList: `);
+    console.log(listData)
+
+    console.log(`TestValue: `);
+    console.log(testData)
 })();
