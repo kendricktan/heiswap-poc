cf3c0e0e47b4f2038012081787f08fdeca5d5167
Kendrick Tan <kendricktan0814@gmail.com>
2019-06-23 23:30:18 +1000

    js h1 working

--- poc/js/bn128/index.js
+++ poc/js/bn128/index.js
@@ -6,7 +6,7 @@
 const EC = require('elliptic');
 const crypto = require('crypto');
 const BN = require('bn.js');
-const { padLeft } = require('web3-utils');
+const { soliditySha3, padLeft } = require('web3-utils');
 
 // Field Modulus
 P = new BN('21888242871839275222246405745257275088696311157297823662689037894645226208583', 10)
@@ -59,14 +59,66 @@ bn128.randomPoint = () => {
     return recurse();
 };
 
-// Point is a type array [BN, BN]
-// Scalar is type BN
-// Returns [BN, BN]
+
+/**
+ * Generates a ring signature for a message given a specific set of public keys
+ * and a secret key which corresponds to the public key at `secretKeyIdx`
+ */
+bn128.ringSign = (
+    message,
+    publicKeys,
+    secretKey,
+    secretKeyIdx
+) => {
+
+}
+
+/**
+ * Generates a ring signature for a message given a specific set of public keys
+ * and a secret key which corresponds to the public key at `secretKeyIdx`
+ * arr: array
+ * returns: string
+ */
+const serialize = (arr) => {
+    return arr.reduce((acc, x, idx) => {
+        // Array
+        if (x.reduce !== undefined) {
+            acc = acc + serialize(x)
+        }
+        else if (x.getX !== undefined && x.getY !== undefined) {
+            acc = acc + padLeft(x.getX().toString(16), 64, '0');
+            acc = acc + padLeft(x.getY().toString(16), 64, '0');
+        }
+        else if (x.toString !== undefined) {
+            acc = acc + padLeft(x.toString(16), 64, '0');
+        }
+
+        return acc
+    }, "")
+}
+
+/*
+ * Let H1: {0, 1}* -> Z_q
+ * Returns an integer representation of the hash of the input
+ * s: string
+ */
+const h1 = (s) => {
+    if (s.indexOf("0x") !== 0) {
+        s = "0x" + s;
+    }
+
+    const h = soliditySha3(s).slice(2); // Remove the '0x'
+    const b = new BN(h, 16);
+
+    return b.mod(N);
+}
 
 sk = bn128.randomGroupScalar()
 pk = bn128.curve.g.mul(sk)
 pk2 = pk.add(pk)
 
+
 console.log(`sk: ${sk}`)
-console.log(`pk: ${pk.getX()}, ${pk.getY()}`)
-console.log(`pk + pk: ${pk2.getX()}, ${pk2.getY()}`)
+// console.log(`pk: ${pk.getX()}, ${pk.getY()}`)
+
+console.log(h1(serialize([sk])))
