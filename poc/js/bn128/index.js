24528e26fab7821b808ecdddc26ba5ad2e280ffa
Kendrick Tan <kendricktan0814@gmail.com>
2019-06-23 22:24:41 +1000

    bn128 ecmul ecadd in js

--- poc/js/bn128/index.js
+++ poc/js/bn128/index.js
@@ -24,3 +24,49 @@ bn128.curve = new EC.curve.short({
     g: ['1', '2']
 })
 
+/**
+ * BN.js reduction context for bn128 curve group's prime modulus
+ * @memberof module:bn128
+ */
+bn128.groupReduction = BN.red(bn128.curve.n);
+
+/**
+ * Get a random BN in the bn128 curve group's reduction context
+ * @method randomGroupScalar
+ * @memberof module:bn128
+ * @returns {BN} BN.js instance
+ */
+bn128.randomGroupScalar = () => {
+    return new BN(crypto.randomBytes(32), 16).toRed(bn128.groupReduction);
+};
+
+/**
+ * Get a random point on the curve
+ * @method randomPoint
+ * @memberof module:bn128
+ * @returns {Point} a random point
+ */
+bn128.randomPoint = () => {
+    const recurse = () => {
+        const x = new BN(crypto.randomBytes(32), 16).toRed(bn128.curve.red);
+        const y2 = x.redSqr().redMul(x).redIAdd(bn128.curve.b);
+        const y = y2.redSqrt();
+        if (y.redSqr(y).redSub(y2).cmp(bn128.curve.a)) {
+            return recurse();
+        }
+        return bn128.curve.point(x, y);
+    }
+    return recurse();
+};
+
+// Point is a type array [BN, BN]
+// Scalar is type BN
+// Returns [BN, BN]
+
+sk = bn128.randomGroupScalar()
+pk = bn128.curve.g.mul(sk)
+pk2 = pk.add(pk)
+
+console.log(`sk: ${sk}`)
+console.log(`pk: ${pk.getX()}, ${pk.getY()}`)
+console.log(`pk + pk: ${pk2.getX()}, ${pk2.getY()}`)
