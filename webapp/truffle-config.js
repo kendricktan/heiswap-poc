8441e994f26e7cb34e5509bc764f1fc306c2687c
Kendrick Tan <kendricktan0814@gmail.com>
2019-06-26 22:29:26 +1000

    webapp boilerplate

---
+++ webapp/truffle-config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  networks: {
+    development: {
+      gas: 4500000,
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    }
+  }
+};
